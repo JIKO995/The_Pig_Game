7d08a5aa18b5c47e9fc96c3379317c986f8d18ca
Gantzos Panos <33899942+JIKO995@users.noreply.github.com>
2022-09-24 11:24:04 +0300

    Add files via upload

---
+++ JavaScript_brains.js
@@ -0,0 +1,91 @@
+'use strict';
+
+//Selecting elements
+const player0EL = document.querySelector('.player--0');
+const player1EL = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+const btnRollDice = document.querySelector('.btn--roll');
+const btnNewGame = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+//final scores
+let scores, activePlayer, currentScore, playing;
+const init = function () {
+  //Starting conditions
+  scores = [0, 0];
+  activePlayer = 0;
+  currentScore = 0;
+  playing = true;
+
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0EL.classList.remove('player--winner');
+  player1EL.classList.remove('player--winner');
+  player0EL.classList.add('player--active');
+  player1EL.classList.remove('player--active');
+};
+
+const switchPlayers = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0EL.classList.toggle('player--active');
+  player1EL.classList.toggle('player--active');
+};
+
+//Rolling dice functionality
+
+init();
+btnRollDice.addEventListener('click', function () {
+  if (playing) {
+    //Generating random dice
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //check for roll and move to the next player
+    if (dice !== 1) {
+      //add dice to curent score
+      currentScore = currentScore + dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //switch to next player
+      switchPlayers();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  //add current score to active players score
+  if (playing) {
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    switchPlayers();
+
+    //check id player's score is 100
+    if (scores[activePlayer] >= 20) {
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      playing = false;
+    } else {
+      switchPlayers();
+    }
+  }
+});
+
+btnNewGame.addEventListener('click', init);
